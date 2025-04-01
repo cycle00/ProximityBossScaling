229f96c864cf82ee2b60e1327d9b6523f9aa121a
Dmitry Vakulenko <dmitry.a.vakulenko@gmail.com>
2025-03-31 22:55:32 -0400

    Added LICENSE and README.md
    Updated description and build files, and added a quick patch to prevent
    any IL tomfoolery when running in singleplayer.

--- ProximityBossScaling.cs
+++ ProximityBossScaling.cs
@@ -8,7 +8,6 @@ using Terraria;
 using Terraria.ModLoader;
 
 namespace ProximityBossScaling {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class ProximityBossScaling : Mod {
 		
 		public override void Load() {
@@ -18,58 +17,65 @@ namespace ProximityBossScaling {
 		}
 
 		private static void ScaleStats_Hook(ILContext il) {
-			try {
-				ILCursor cursor = new ILCursor(il);
-				ILLabel label = il.DefineLabel();
-				cursor.GotoNext(i => i.MatchLdcI4(1));
-				cursor.Index += 3;
+			if (Main.netMode != 0) { // no point in doing any of this if player is in singleplayer
+				try {
+					ILCursor cursor = new ILCursor(il);
+					ILLabel label = il.DefineLabel();
+					cursor.GotoNext(i => i.MatchLdcI4(1));
+					cursor.Index += 3; // place cursor right before the code which pushes the number of total players to the stack
 
-				cursor.EmitLdarg0();
-				cursor.EmitDelegate<Func<NPC, int>>(npc => {
-					int num = 0;
-					foreach (var player in Main.ActivePlayers) {
-						if ((int)Math.Abs(npc.position.X - player.position.X) < (int)((double)NPC.sWidth * 2.1) && (int)Math.Abs(npc.position.Y - player.position.Y) < (int)((double)NPC.sHeight * 2.1)) {
-							num++;
+					cursor.EmitLdarg0(); // push a the current NPC instance onto the stack since next function call requires the current instance
+					cursor.EmitDelegate<Func<NPC, int>>(npc => {
+						int num = 0;
+						foreach (var player in Main.ActivePlayers) {
+							if ((int)Math.Abs(npc.position.X - player.position.X) < (int)((double)NPC.sWidth * 2.1) && (int)Math.Abs(npc.position.Y - player.position.Y) < (int)((double)NPC.sHeight * 2.1)) {
+								num++;
+							}
 						}
-					}
-					return num;
-				});
-				cursor.EmitBr(label);
-				
-				cursor.GotoNext(i => i.MatchDup());
-				cursor.MarkLabel(label);
+						return num;
+					}); // push the detected number of players to the stack
+					cursor.EmitBr(label); // skip the original code which pushes the total number of players to the stack
+					
+					cursor.GotoNext(i => i.MatchDup());
+					cursor.MarkLabel(label);
 
 #pragma warning disable CS0168
-			} catch (Exception e) {
+				} catch (Exception e) {
 #pragma warning restore
-				MonoModHooks.DumpIL(ModContent.GetInstance<ProximityBossScaling>(), il); // bugged in windows lmfao
+					MonoModHooks.DumpIL(ModContent.GetInstance<ProximityBossScaling>(), il); // bugged in windows lmfao
+				}
 			}
 		}
 
-		private static void NPCPosition_Hook(ILContext il) {
-			try {
-				ILCursor cursor = new ILCursor(il);
-				ILLabel beforeSetDefaults = cursor.DefineLabel();
-				ILLabel afterSetDefaults = cursor.DefineLabel();
-				ILLabel afterPosition = cursor.DefineLabel();
+		private static void NPCPosition_Hook(ILContext il) { 
+			if (Main.netMode != 0) {	
+				// make the NPC get positioned before SetDefaults gets run so that ScaleStats has access to the NPC's spawn position
+				try {
+					ILCursor cursor = new ILCursor(il);
+					ILLabel beforeSetDefaults = cursor.DefineLabel();
+					ILLabel afterSetDefaults = cursor.DefineLabel();
+					ILLabel afterPosition = cursor.DefineLabel();
 
-				cursor.GotoNext(i => i.MatchStelemRef());
-				cursor.Index++;
-				cursor.EmitBr(afterSetDefaults);
-				cursor.MarkLabel(beforeSetDefaults);
+					cursor.GotoNext(i => i.MatchStelemRef());
+					cursor.Index++;
+					cursor.EmitBr(afterSetDefaults); 
+					cursor.MarkLabel(beforeSetDefaults);
 
-				cursor.Index += 8;
-				cursor.EmitBr(afterPosition);
-				cursor.MarkLabel(afterSetDefaults);
+					cursor.Index += 8;
+					cursor.EmitBr(afterPosition); 
+					cursor.MarkLabel(afterSetDefaults);
 
-				cursor.Index += 34;
-				cursor.EmitBr(beforeSetDefaults);
-				cursor.MarkLabel(afterPosition);
+					cursor.Index += 34;
+					cursor.EmitBr(beforeSetDefaults);
+					cursor.MarkLabel(afterPosition);
 
+					// jump nonsense: jump to after SetDefaults -> execute all code until end of positioning 
+					// -> jump to start of SetDefaults -> call SetDefaults -> jump to after positioning
 #pragma warning disable CS0168
-			} catch (Exception e) {
+				} catch (Exception e) {
 #pragma warning restore
-				MonoModHooks.DumpIL(ModContent.GetInstance<ProximityBossScaling>(), il);
+					MonoModHooks.DumpIL(ModContent.GetInstance<ProximityBossScaling>(), il);
+				}
 			}
 		}
 	}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Proximity Boss Scaling
+Have you ever been on a server with your friends trying to farm this one boss for a specific drop, but friend A is out mining, friend B is on the other side of the world fishing, and friend C is afk at spawn? Well I certainly have. This makes it a huge pain to kill any decently difficult boss because of just how much longer it takes. So I created this mod to deal with the issue.
+## What it does
+When a boss spawns, it checks to see how many players are within a certain area of its spawnpoint and scales its HP based on that number instead of the total number of players in the server. 
+
+There's a very obvious issue with this approach; it allows players to cheese bosses by spawning it with one player nearby, then having everyone else fight the boss together after it was spawned. I was thinking of ways to try to prevent this, but then I realized that in vanilla Terraria you can do the same by spawning a boss, then having everyone else log in to the server afterwards. So if you really want to use this mod to cheese bosses, be my guest. But at that point, why not just find a mod that disables multiplayer boss scaling altogether?
+## Bugs
+This mod likely has many bugs I've yet to discover, and while I'm not sure if I intend to actively maintain it or not, feel free to open an issue or a pull request and I'll gladly look at it.
